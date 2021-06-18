bf532f8f588aeaaa0bb476c4a9dadffdb5f19f9a
Jeferson <jroliveira.oliveira301@gmail.com>
2021-06-18 15:36:38 +0200

    sdm660-common: Build motocamera
    Change-Id: I451a6ca2f70c34113507917a806fde01fcb3e8d9

--- common.mk
+++ common.mk
@@ -90,8 +90,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@3.2-impl \
     libbson.vendor \
-    libxml2 \
-    Snap
+    libxml2
 
 # Codec2 modules
 PRODUCT_PACKAGES += \
@@ -231,6 +230,10 @@ PRODUCT_COPY_FILES += \
 # MotoActions
 PRODUCT_PACKAGES += \
     MotoActions
+
+# Moto Camera 2
+PRODUCT_PACKAGES += \
+    MotCamera2
 
 # Network
 PRODUCT_PACKAGES += \
